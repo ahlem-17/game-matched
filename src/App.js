fad9024dddb5683435bdb3ee7d9dc287a00e47f7
ahlem-20 <ahlemmh123@gmail.com>
2022-10-20 12:38:47 +0300

    final

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Board } from './features/board/Board';
+import { Score } from './features/score/Score';
+import { setBoard, resetCards } from './features/board/boardSlice.js';
+
+const App = () => {
+
+  const dispatch = useDispatch();
+  
+  const startGameHandler = () => {
+    // Add action dispatch below
+    dispatch(setBoard());
+  };
+  const tryAgainHandler = () => {
+    // Add action dispatch below
+    dispatch(resetCards());
+  };
+  return (
+    <div className="App">
+    <Score />
+     <Board />
+     <footer className='footer'>
+       <button onClick={startGameHandler}   className='start-button'>Start Game</button>
+       <button onClick={tryAgainHandler} className='try-new-pair-button'>Try New Pair</button>
+     </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/features/board/cardRow/card/Card.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectVisibleIDs,
+flipCard,
+selectMatchedIDs } from '../../boardSlice';
+
+let cardLogo = "https://images.unsplash.com/photo-1553481187-be93c21490a9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80";
+
+export const Card = ({ id, contents }) => {
+  // Add selected data and dispatch variables below
+  const visibleIDs = useSelector(selectVisibleIDs);
+  const matchedIDs = useSelector(selectMatchedIDs);
+  const dispatch = useDispatch();
+  
+  // flip card action
+  const flipHandler = (id) => {
+    // Add action dispatch below
+    dispatch(flipCard(id));
+  };
+
+  let cardStyle = 'resting'
+  let click = () => flipHandler(id);
+  
+  let cardText = (
+    <img src={cardLogo} className="logo-placeholder" alt="Card option" />
+  );
+
+  // 1st if statement
+  // implement card id array membership check
+  if (visibleIDs.includes(id) || matchedIDs.includes(id))   {
+    cardText = contents;
+    click = () => {};
+  }
+
+  // 2nd if statement
+  // implement card id array membership check
+  if (matchedIDs.includes(id)) {
+    cardStyle = 'matched';
+  }
+
+  // 3rd if statement
+  // implement number of flipped cards check
+  if (visibleIDs.length == 2) {
+    click = () => {};
+  }
+
+  return (
+    <button onClick={click} className={`card ${cardStyle}`}>
+      {cardText}
+    </button>
+  );
+};
